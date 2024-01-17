aed70e3097f226c91476dcacb4bc990c4bf99b25
eddieavlis <146478309+eddieavlis@users.noreply.github.com>
2024-01-17 18:13:27 -0300

    Update Customer.kt

--- src/main/kotlin/com/eddie/credit/application/system/entity/Customer.kt
+++ src/main/kotlin/com/eddie/credit/application/system/entity/Customer.kt
@@ -1,5 +1,6 @@
 package com.eddie.credit.application.system.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 
@@ -11,11 +12,15 @@ data class Customer(
     @Column(nullable = false, unique = true) var cpf: String = "",
     @Column(nullable = false, unique = true) var email: String = "",
     @Column(nullable = false) var income: BigDecimal = BigDecimal.ZERO,
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(nullable = false) var password: String = "",
     @Column(nullable = false) @Embedded var address: Address = Address(),
-    @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY,
-        cascade = arrayOf(CascadeType.REMOVE, CascadeType.PERSIST),
-        mappedBy = "customer")
+    @Column(nullable = false) @OneToMany(
+        fetch = FetchType.LAZY,
+        cascade = [CascadeType.REMOVE, CascadeType.PERSIST],
+        mappedBy = "customer"
+    )
     var credits: List<Credit> = mutableListOf(),
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null,
+    @Transient val zipCode: String = address.zipCode,
 )
