2ab9b30753b771d9536e5515f704daf35c6e5be2
eddieavlis <146478309+eddieavlis@users.noreply.github.com>
2024-01-17 18:14:28 -0300

    Update Address.kt

--- src/main/kotlin/com/eddie/credit/application/system/entity/Address.kt
+++ src/main/kotlin/com/eddie/credit/application/system/entity/Address.kt
@@ -6,5 +6,5 @@ import jakarta.persistence.Embeddable
 @Embeddable
 data class Address(
     @Column(nullable = false) var zipCode: String = "",
-    @Column(nullable = false) var street: String = ""
+    var street: String = ""
 )
