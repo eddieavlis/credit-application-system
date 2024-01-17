34067f8bd2bcfaff4fc86103ce07dfc572706dc9
eddieavlis <146478309+eddieavlis@users.noreply.github.com>
2024-01-17 18:16:24 -0300

    Update CreditViewList.kt

--- src/main/kotlin/com/eddie/credit/application/system/dto/response/CreditViewList.kt
+++ src/main/kotlin/com/eddie/credit/application/system/dto/response/CreditViewList.kt
@@ -1,4 +1,4 @@
-package com.eddie.credit.application.system.dto
+package com.eddie.credit.application.system.dto.response
 
 import com.eddie.credit.application.system.entity.Credit
 import java.math.BigDecimal
