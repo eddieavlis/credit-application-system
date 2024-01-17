c1057390ba1fbabbd4d25cf73443bedf018bdda1
eddieavlis <146478309+eddieavlis@users.noreply.github.com>
2024-01-17 18:13:55 -0300

    Update Credit.kt

--- src/main/kotlin/com/eddie/credit/application/system/entity/Credit.kt
+++ src/main/kotlin/com/eddie/credit/application/system/entity/Credit.kt
@@ -6,12 +6,14 @@ import java.time.LocalDate
 import java.util.*
 
 @Entity
-//@Table(name = "Credito")
-data class Credit (
-    @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
-    @Column(nullable = false) val creditValue: BigDecimal = BigDecimal.ZERO,
-    @Column(nullable = false) val dayFirstInstallment: LocalDate,
-    @Column(nullable = false) val numberOfInstallments: Int = 0,
-    @Enumerated val status: Status = Status.IN_PROGRESS,
-    @ManyToOne var customer: Customer? = null,
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null)
+data class Credit(
+    @Column(nullable = false, unique = true) var creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false) var creditValue: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false) var numberOfInstallments: Int = 0,
+    @Enumerated(EnumType.STRING) val status: Status = Status.IN_PROGRESS,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    var customer: Customer? = null,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
+    val dayFirstInstallment: LocalDate
+)
