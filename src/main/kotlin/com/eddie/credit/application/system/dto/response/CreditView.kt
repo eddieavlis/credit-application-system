6c04b93e002bd6409c484793126985f0ac26a680
eddieavlis <146478309+eddieavlis@users.noreply.github.com>
2024-01-17 18:16:50 -0300

    Update CreditView.kt

--- src/main/kotlin/com/eddie/credit/application/system/dto/response/CreditView.kt
+++ src/main/kotlin/com/eddie/credit/application/system/dto/response/CreditView.kt
@@ -1,4 +1,4 @@
-package com.eddie.credit.application.system.dto
+package com.eddie.credit.application.system.dto.response
 
 import com.eddie.credit.application.system.entity.Credit
 import com.eddie.credit.application.system.entity.Status
