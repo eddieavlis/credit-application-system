44d68f782e5e1d056011601dab27d27b1729e8a0
eddieavlis <146478309+eddieavlis@users.noreply.github.com>
2024-01-17 15:38:44 -0300

    Update CreditRepositoryTest.kt

--- src/test/kotlin/com/eddie/credit/application/system/repository/CreditRepositoryTest.kt
+++ src/test/kotlin/com/eddie/credit/application/system/repository/CreditRepositoryTest.kt
@@ -1,4 +1,99 @@
 package com.eddie.credit.application.system.repository
 
+import com.eddie.credit.application.system.entity.Address
+import com.eddie.credit.application.system.entity.Credit
+import com.eddie.credit.application.system.entity.Customer
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.test.context.ActiveProfiles
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.Month
+import java.util.*
+
+@ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CreditRepositoryTest {
+    @Autowired lateinit var creditRepository: CreditRepository
+    @Autowired lateinit var testEntityManager: TestEntityManager
+
+    private lateinit var customer: Customer
+    private lateinit var credit1: Credit
+    private lateinit var credit2: Credit
+
+    @BeforeEach
+    fun setup () {
+        customer = testEntityManager.persist(buildCustomer())
+        credit1 = testEntityManager.persist(buildCredit(customer = customer))
+        credit2 = testEntityManager.persist(buildCredit(customer = customer))
+    }
+
+    @Test
+    fun `should find credit by credit code`() {
+        //given
+        val creditCode1 = UUID.fromString("6ddb3b05-b061-4ddc-818d-aee5940cab08")
+        val creditCode2 = UUID.fromString("179cc268-e133-45f2-bcb5-6390c21513f2")
+        credit1.creditCode = creditCode1
+        credit2.creditCode = creditCode2
+        //when
+        val fakeCredit1: Credit = creditRepository.findByCreditCode(creditCode1)!!
+        val fakeCredit2: Credit = creditRepository.findByCreditCode(creditCode2)!!
+        //then
+        Assertions.assertThat(fakeCredit1).isNotNull
+        Assertions.assertThat(fakeCredit2).isNotNull
+        Assertions.assertThat(fakeCredit1).isSameAs(credit1)
+        Assertions.assertThat(fakeCredit2).isSameAs(credit2)
+    }
+
+    @Test
+    fun `should find all credits by customer id`() {
+        //given
+        val customerId: Long = 1L
+        //when
+        val creditList: List<Credit> = creditRepository.findAllByCustomerId(customerId)
+        //then
+        Assertions.assertThat(creditList).isNotEmpty
+        Assertions.assertThat(creditList.size).isEqualTo(2)
+        Assertions.assertThat(creditList).contains(credit1, credit2)
+    }
+
+    private fun buildCredit(
+        creditValue: BigDecimal = BigDecimal.valueOf(9000),
+        dayFirstInstallment: LocalDate = LocalDate.of(2024, Month.APRIL, 22),
+        numberOfInstallments: Int = 6,
+        customer: Customer
+    ): Credit = Credit(
+        creditValue = creditValue,
+        dayFirstInstallment = dayFirstInstallment,
+        numberOfInstallments = numberOfInstallments,
+        customer = customer
+    )
+
+    private fun buildCustomer(
+        firstName: String = "Antonio",
+        lastName: String = "Silva",
+        cpf: String = "94692505834",
+        email: String = "antonio@hotmail.com",
+        password: String = "1234_veryGood",
+        zipCode: String = "06298030",
+        street: String = "Rua do Antonio",
+        income: BigDecimal = BigDecimal.valueOf(9000),
+    ) = Customer(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        password = password,
+        address = Address(
+            zipCode = zipCode,
+            street = street,
+        ),
+        income = income,
+    )
 }
